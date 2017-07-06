44c900776e2f849b9c4f3b617bdf6b5cffcae3cb
msx752 <aslim@outlook.com.tr>
2017-07-06 14:38:33 +0300

    v1.0.0.3

--- src/BaseMvvm.Droid/Properties/AssemblyInfo.cs
+++ src/BaseMvvm.Droid/Properties/AssemblyInfo.cs
@@ -3,14 +3,13 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Android.App;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// General Information about an assembly is controlled through the following set of attributes.
+// Change these attribute values to modify the information associated with an assembly.
 [assembly: AssemblyTitle("BaseMvvm.Droid")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("BaseMvvm.XamarinForms all-in-one easy mvvm implementation")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BaseMvvm.Droid")]
+[assembly: AssemblyCompany("Mustafa Salih ASLIM")]
+[assembly: AssemblyProduct("BaseMvvm.XamarinForms")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -18,13 +17,9 @@ using Android.App;
 
 // Version information for an assembly consists of the following four values:
 //
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
+// Major Version Minor Version Build Number Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// You can specify all the values or you can default the Build and Revision Numbers by using the '*'
+// as shown below: [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
--- src/BaseMvvm/Properties/AssemblyInfo.cs
+++ src/BaseMvvm/Properties/AssemblyInfo.cs
@@ -3,28 +3,23 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("BaseMvvm")]
-[assembly: AssemblyDescription("")]
+// General Information about an assembly is controlled through the following set of attributes.
+// Change these attribute values to modify the information associated with an assembly.
+[assembly: AssemblyTitle("BaseMvvm.XamarinForms")]
+[assembly: AssemblyDescription("BaseMvvm.XamarinForms all-in-one easy mvvm implementation")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BaseMvvm")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Mustafa Salih ASLIM")]
+[assembly: AssemblyProduct("BaseMvvm.XamarinForms")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
+// Major Version Minor Version Build Number Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// You can specify all the values or you can default the Build and Revision Numbers by using the '*'
+// as shown below: [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
