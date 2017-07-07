de0bd9b3144e56403f2761a94db19f3053a9894b
msx752 <aslim@outlook.com.tr>
2017-07-07 15:28:45 +0300

    no message

--- CreateNugetPackage.ps1
+++ CreateNugetPackage.ps1
@@ -1,10 +1,11 @@
 ﻿if($($env:APPVEYOR))
 { 
 	$SVersion = $($env:APPVEYOR_BUILD_VERSION);
-    Write-Host "BaseMvvm.XamarinForms Version:" $SVersion;
-.\nuget pack BaseMvvm.XamarinForms.nuspec -version $SVersion;
-	Push-AppveyorArtifact "BaseMvvm.XamarinForms." $SVersion ".nupkg";
+    Write-Host "BaseMvvm.XamarinForms Version:$SVersion";
+.\nuget pack BaseMvvm.XamarinForms.nuspec -version $SVersion
+
+    Push-AppveyorArtifact "C:\projects\basemvvm-xamarinforms\BaseMvvm.XamarinForms.$SVersion.nupkg" -name "NugetPackage1";
 }
 else{
-    Write-Host "AppVeyor was not detected"
+    Write-Host "AppVeyor was not detected";
 }
