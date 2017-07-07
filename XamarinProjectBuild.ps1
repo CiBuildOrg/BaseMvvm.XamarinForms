c1fd19d223128a57f420d9d15b90701abb1b676a
msx752 <aslim@outlook.com.tr>
2017-07-07 12:45:53 +0300

    nuget implementation

--- XamarinProjectBuild.ps1
+++ XamarinProjectBuild.ps1
@@ -1,3 +1,4 @@
-﻿$msbuild = "C:\Program Files (x86)\MSBuild\14.0\Bin\amd64\MSBuild.exe"
+﻿.\nuget restore BaseMvvm.XamarinForms.sln
+$msbuild = "C:\Program Files (x86)\MSBuild\14.0\Bin\amd64\MSBuild.exe"
 & $msbuild BaseMvvm.XamarinForms.sln /property:Configuration=Release /target:Clean /target:Build
 .\nuget pack BaseMvvm.XamarinForms.nuspec
