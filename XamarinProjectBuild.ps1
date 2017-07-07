b6ef9746868a8794d433d199151ba7257a989f8f
msx752 <aslim@outlook.com.tr>
2017-07-07 15:51:51 +0300

    no message

--- XamarinProjectBuild.ps1
+++ XamarinProjectBuild.ps1
@@ -2,11 +2,12 @@
 {
     $SVersion = $($env:APPVEYOR_BUILD_VERSION);
     Write-Host "Project Build Version: " $SVersion;
-.\SlnVersionChange $SVersion
+#.\SlnVersionChange $SVersion
 
 .\nuget restore BaseMvvm.XamarinForms.sln
 	$msbuild = "C:\Program Files (x86)\MSBuild\14.0\Bin\amd64\MSBuild.exe"
-& $msbuild BaseMvvm.XamarinForms.sln /property:Configuration=Release
+& $msbuild BaseMvvm.XamarinForms.sln /property:Configuration=Release /target:Clean /target:Build
+
 .\CreateNugetPackage.ps1
 }
 else{
