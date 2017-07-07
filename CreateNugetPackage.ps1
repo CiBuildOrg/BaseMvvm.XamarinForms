f7f9d725f5722b406b4d1b7737958854dcf95367
msx752 <aslim@outlook.com.tr>
2017-07-07 14:19:07 +0300

    nuget implementatio #2

---
+++ CreateNugetPackage.ps1
@@ -0,0 +1,9 @@
+﻿$path = "src\BaseMvvm\Properties\AssemblyInfo.cs" 
+$pattern = '\[assembly: AssemblyVersion\("(\d\.\d\.\d\.\d)"\)\]'
+$content = Get-Content $path;
+$rslt = $content -match $pattern;
+$SlnVersion = $rslt -replace '[assembly: AssemblyVersion\(")]' ,"";
+$SlnVersion = $SlnVersion -replace '\[' ,"";
+$SlnVersion = $SlnVersion -replace '\]' ,"";
+Write-Host "BaseMvvm.XamarinForms Version:" $SlnVersion;
+.\nuget pack BaseMvvm.XamarinForms.nuspec -version $SlnVersion;
