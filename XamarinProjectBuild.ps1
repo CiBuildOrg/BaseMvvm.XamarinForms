f2b14a79bcddcea7a2e9ff613b14a496f365864b
msx752 <aslim@outlook.com.tr>
2017-07-07 16:22:18 +0300

    xamarin builder sample

--- XamarinProjectBuild.ps1
+++ XamarinProjectBuild.ps1
@@ -1,15 +1,18 @@
 ﻿if($($env:APPVEYOR))
-{
+{ 
     $SVersion = $($env:APPVEYOR_BUILD_VERSION);
-    Write-Host "Project Build Version: " $SVersion;
-#.\SlnVersionChange $SVersion
+    Write-Host "BaseMvvm.XamarinForms Version:$SVersion";
 
-.\nuget restore BaseMvvm.XamarinForms.sln
-	$msbuild = "C:\Program Files (x86)\MSBuild\14.0\Bin\amd64\MSBuild.exe"
-& $msbuild BaseMvvm.XamarinForms.sln /property:Configuration=Release /target:Clean /target:Build
+.\nuget restore BaseMvvm.XamarinForms.sln;
+    $msbuild = "C:\Program Files (x86)\MSBuild\14.0\Bin\amd64\MSBuild.exe"
+    & $msbuild BaseMvvm.XamarinForms.sln /property:Configuration=Release
 
-.\CreateNugetPackage.ps1
+cd C:\projects\basemvvm-xamarinforms\
+    .\nuget pack BaseMvvm.XamarinForms.nuspec -version $SVersion;
+
+    Push-AppveyorArtifact "C:\projects\basemvvm-xamarinforms\BaseMvvm.XamarinForms.$SVersion.nupkg" -FileName "BaseMvvm.XamarinForms.$SVersion.nupkg" -DeploymentName "NugetPackage1";
 }
-else{
-    Write-Host "AppVeyor was not detected"
+else
+{
+    Write-Host "AppVeyor was not detected";
 }
