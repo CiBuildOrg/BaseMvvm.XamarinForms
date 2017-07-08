3ff749df6966802e522157d10e9bc5b28acd7e89
msx752 <aslim@outlook.com.tr>
2017-07-08 11:18:26 +0300

    no message

--- XamarinProjectBuild.ps1
+++ XamarinProjectBuild.ps1
@@ -7,10 +7,9 @@
     $msbuild = "C:\Program Files (x86)\MSBuild\14.0\Bin\amd64\MSBuild.exe"
     & $msbuild BaseMvvm.XamarinForms.sln /property:Configuration=Release
 
-cd C:\projects\basemvvm-xamarinforms\
     .\nuget pack BaseMvvm.XamarinForms.nuspec -version $SVersion;
 
-    Push-AppveyorArtifact "C:\projects\basemvvm-xamarinforms\BaseMvvm.XamarinForms.$SVersion.nupkg" -FileName "BaseMvvm.XamarinForms.$SVersion.nupkg" -DeploymentName "NugetPackage1";
+    Push-AppveyorArtifact "$($env:APPVEYOR_BUILD_FOLDER)\BaseMvvm.XamarinForms.$SVersion.nupkg" -FileName "BaseMvvm.XamarinForms.$SVersion.nupkg" -DeploymentName "NugetPackage1";
 }
 else
 {
